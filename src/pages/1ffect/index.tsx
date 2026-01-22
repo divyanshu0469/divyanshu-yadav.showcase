8c529a344a25fc0780dbc62f4afc8cbf809181a8
divyanshu0469 <divyanshuthegreat101@gmail.com>
2026-01-22 22:59:23 +0530

    fix: made changes for per-page asset loading

--- src/pages/1ffect/index.tsx
+++ src/pages/1ffect/index.tsx
@@ -1,11 +1,24 @@
-import { useState } from "react";
+import { useState, ReactElement } from "react";
+import { Anton, Inter } from "next/font/google";
 import AnimatedRedLines from "@/components/1ffect/AnimatedRedLines";
 import AnimatedHeader from "@/components/1ffect/AnimatedHeader";
 import Preloader from "@/components/1ffect/Preloader";
 import styles from "./1ffect.module.css";
 import ToolBar from "@/components/common/ToolBar";
+import type { NextPageWithLayout } from "@/pages/_app";
 
-export default function Effect1() {
+const anton = Anton({
+  weight: "400",
+  subsets: ["latin"],
+  variable: "--font-anton",
+});
+
+const inter = Inter({
+  subsets: ["latin"],
+  variable: "--font-inter",
+});
+
+const Effect1: NextPageWithLayout = () => {
   const [animationsStarted, setAnimationsStarted] = useState(false);
 
   const handlePreloaderComplete = () => {
@@ -26,4 +39,14 @@ export default function Effect1() {
       />
     </div>
   );
-}
+};
+
+Effect1.getLayout = (page: ReactElement) => {
+  return (
+    <div className={`${anton.variable} ${inter.variable}`}>
+      {page}
+    </div>
+  );
+};
+
+export default Effect1;
